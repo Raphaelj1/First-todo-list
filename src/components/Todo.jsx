0ddbbce8bc2c353a1948677c304a214df345000e
Raphaelj <raphaeljames897@gmail.com>
2024-10-16 18:43:09 +0100

    update

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -82,7 +82,7 @@ function Todo() {
             </div>
 
             <div>
-                <form action="" onSubmit={handleSubmit} className='flex justify-between items-center overflow-hidden my-4 md:my-7 bg-gray-200 rounded-full'>
+                <form action="" onSubmit={handleSubmit} className='flex justify-between items-center overflow-hidden my-4 md:my-7 bg-gray-200 text-base rounded-full'>
                     <input
                         type="text"
                         ref={taskInput}
